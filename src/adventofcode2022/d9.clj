7bf89353a35e3b276def403b81f591f6ff35cb31
Joe Littlejohn <joelittlejohn@gmail.com>
2022-12-17 22:40:12 +0000

    Day 9 part 2

--- src/adventofcode2022/d9.clj
+++ src/adventofcode2022/d9.clj
@@ -1,5 +1,6 @@
 (ns adventofcode2022.d9
   (:require [clojure.java.io :as io]
+            [clojure.math :refer [signum]]
             [clojure.string :as str]))
 
 (defn- parse
@@ -21,22 +22,42 @@
 (def movement-vectors
   {"L" [-1 0] "R" [1 0] "U" [0 1] "D" [0 -1]})
 
+(defn- new-tail
+  [new-head old-tail]
+  (let [[headx heady] new-head
+        [old-tailx old-taily] old-tail
+        [diffx diffy] (subtract new-head old-tail)]
+    (cond
+      (and (> (abs diffx) 1) (> (abs diffy) 1)) [(+ old-tailx (int (signum diffx))) (+ old-taily (int (signum diffy)))]
+      (> (abs diffx) 1) [(+ old-tailx (int (signum diffx))) heady]
+      (> (abs diffy) 1) [headx (+ old-taily (int (signum diffy)))]
+      :else old-tail)))
+
 (defn- move
   [states [d n :as _move]]
   (if (zero? n)
     states
-    (let [{:keys [head tail]} (last states)
+    (let [rope (last states)
           head-move-vec (movement-vectors d)
-          new-head (add head head-move-vec)
-          [diffx diffy] (subtract new-head tail)
-          new-tail (if (or (> (abs diffx) 1) (> (abs diffy) 1))
-                     head
-                     tail)]
-      (move (conj states {:head new-head :tail new-tail}) [d (dec n)]))))
+          new-head (add (first rope) head-move-vec)
+          new-rope (reduce (fn [new-rope old-tail]
+                             (let [new-head (last new-rope)
+                                   new-tail (new-tail new-head old-tail)]
+                               (conj new-rope new-tail)))
+                           [new-head]
+                           (next rope))]
+      (move (conj states new-rope) [d (dec n)]))))
 
-(->> (reduce move [{:head [0 0] :tail [0 0]}] input)
-     (map :tail)
+(->> (reduce move [(repeat 2 [0 0])] input)
+     (map last)
      set
      count)
 
 ;; => 5960
+
+(->> (reduce move [(repeat 10 [0 0])] input)
+     (map last)
+     set
+     count)
+
+;; => 2327
